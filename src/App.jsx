cca441cfa4cf7f81b683892fbdbc7c6fe1e696c4
sumit-616 <sumit616794@gmail.com>
2025-02-14 00:07:38 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import Header from './components/Header';
 import About from './components/About';
-import Navbar from './components/Navbar';
 
 export const App = () => {
   return (
     <div className='w-full overflow-hidden'>
         <Header />
+        <About />
     </div>
   )
 }
