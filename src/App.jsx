c47fb228b224a6dbb20edd477b072b811380dcba
sumit-616 <sumit616794@gmail.com>
2025-02-13 12:40:31 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from './components/Header';
+import About from './components/About';
+import Navbar from './components/Navbar';
+
+export const App = () => {
+  return (
+    <div className='w-full overflow-hidden'>
+        <Header />
+    </div>
+  )
+}
+
+export default App;
