9f96518d8965f5908976481a0ea6bbb450572d9a
sumit-616 <sumit616794@gmail.com>
2025-02-15 22:36:45 +0530

    Project Section updating

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import Header from './components/Header';
 import About from './components/About';
-
+import Projects from './components/Projects';
 export const App = () => {
   return (
     <div className='w-full overflow-hidden'>
         <Header />
         <About />
+        <Projects />
     </div>
   )
 }
