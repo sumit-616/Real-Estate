b3ef6e7b4e7bddaef4b581d21316c1e367b4c08c
sumit-616 <sumit616794@gmail.com>
2025-02-16 22:44:55 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
-export default defineConfig(({ mode }) => ({
-  plugins: [react()],
-  base: mode === "development" ? "/" : "/real-estate/",
-}))
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react(), tailwindcss()],
+  base: "/real-estate/"
+})
