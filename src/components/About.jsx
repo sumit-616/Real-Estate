abe96aa2f27da7c68900fdbeb14c18034ef91e5f
sumit-616 <sumit616794@gmail.com>
2025-02-17 12:22:48 +0530

    bug fix

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,11 +4,7 @@ import { motion } from 'framer-motion'
 
 const About = () => {
   return (
-    <motion.div 
-      initial={{ opacity: 0, x: 200 }}
-      transition={{ duration: 1 }}
-      whileInView={{opacity: 1, x: 0}}
-      viewport={{once: true}}
+    <div 
     className='flex flex-col items-center justify-center container mx-auto p-14 md:px-20 lg:px-32 w-full overflow-hidden' id='About'>
       <h1 className='text-2xl sm:text-4xl font-bold mb-2'>About <span className='underline underline-offset-4 decoration-1 under font-light'>Our Brand</span></h1>
       <p className='text-gray-500 max-w-80 text-center mb-8'>Passionate about Properties, Dedicate to Your Vision</p>
@@ -37,7 +33,7 @@ const About = () => {
           <button className='cursor-pointer bg-blue-600 text-white px-8 py-2 rounded'>Learn More</button>
         </div>
       </div>
-    </motion.div>
+    </div>
   )
 }
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import Navbar from './Navbar'
+import headerImg from '../assets/header_img.png';
 import { motion } from "framer-motion"
 
 const Header = () => {
   return (
-    <div className='min-h-screen mb-4 bg-cover bg-center flex items-center w-full overflow-hidden' style={{backgroundImage: "url('/header_img.png')"}} id='Header'>
+    <div className='min-h-screen mb-4 bg-cover bg-center flex items-center w-full overflow-hidden' style={{backgroundImage: `url(${headerImg})`}} id='Header'>
         <Navbar />
           <motion.div 
           initial={{ opacity: 0, y: 100 }}
